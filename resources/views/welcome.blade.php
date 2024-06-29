55490c60a8c62085b4c4ab2e87a38249d7e2dc29
angelyovanny <angel_yovany@hotmail.com>
2024-06-29 19:49:03 -0400

    colocar negrita

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,11 +36,9 @@
                                         Sitio de trabajo
                                     </a>
                                 @else
-                                    <a
-                                        href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        INICIAR SECIÓN
+                                <a href="{{ route('login') }}"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                    <strong>INICIAR SECCIÓN</strong>
                                     </a>
 
                                     @if (Route::has('register'))
