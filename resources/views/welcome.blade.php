973fc52c1deeac1245f1dc920cd6bd03949e3b23
angelyovanny <angel_yovany@hotmail.com>
2024-06-29 19:52:15 -0400

    cambiar el registro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,17 +36,17 @@
                                         Sitio de trabajo
                                     </a>
                                 @else
-                                <a href="{{ route('login') }}"
+                                <a 
+                                    href="{{ route('login') }}"
                                     class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                                     <strong>INICIAR SECCIÓN</strong>
-                                    </a>
+                                        </a>
 
                                     @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            REGISTRARSE
+                                <a
+                                    href="{{ route('register') }}"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                    <strong>REGISTRARSE</strong>                                            
                                         </a>
                                     @endif
                                 @endauth
