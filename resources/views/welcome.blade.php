809a11902f663d02964e47d838daff2ab21582fe
angelyovanny <angel_yovany@hotmail.com>
2024-06-29 19:31:53 -0400

    modificacion de reseña historica

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,15 +113,15 @@
                                     <svg class="size-5 sm:size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><g fill="#FF2D20"><path d="M24 8.25a.5.5 0 0 0-.5-.5H.5a.5.5 0 0 0-.5.5v12a2.5 2.5 0 0 0 2.5 2.5h19a2.5 2.5 0 0 0 2.5-2.5v-12Zm-7.765 5.868a1.221 1.221 0 0 1 0 2.264l-6.626 2.776A1.153 1.153 0 0 1 8 18.123v-5.746a1.151 1.151 0 0 1 1.609-1.035l6.626 2.776ZM19.564 1.677a.25.25 0 0 0-.177-.427H15.6a.106.106 0 0 0-.072.03l-4.54 4.543a.25.25 0 0 0 .177.427h3.783c.027 0 .054-.01.073-.03l4.543-4.543ZM22.071 1.318a.047.047 0 0 0-.045.013l-4.492 4.492a.249.249 0 0 0 .038.385.25.25 0 0 0 .14.042h5.784a.5.5 0 0 0 .5-.5v-2a2.5 2.5 0 0 0-1.925-2.432ZM13.014 1.677a.25.25 0 0 0-.178-.427H9.101a.106.106 0 0 0-.073.03l-4.54 4.543a.25.25 0 0 0 .177.427H8.4a.106.106 0 0 0 .073-.03l4.54-4.543ZM6.513 1.677a.25.25 0 0 0-.177-.427H2.5A2.5 2.5 0 0 0 0 3.75v2a.5.5 0 0 0 .5.5h1.4a.106.106 0 0 0 .073-.03l4.54-4.543Z"/></g></svg>
                                 </div>
 
-                                <div class="pt-3 sm:pt-5">
+                                    <div class="pt-3 sm:pt-5"> 
                                     <h2 class="text-xl font-semibold text-black dark:text-white">Nuestra Historia</h2>
 
                                     <p class="mt-4 text-sm/relaxed">
                                         En el año 2007, Alberto Nieto fundó RECEVECA en San Cristóbal, estado Táchira. La empresa nació con la visión de brindar protección y tranquilidad a los conductores y propietarios de vehículos en la región. A lo largo de los años, la empresa se consolidó como un referente en el mercado de seguros de responsabilidad civil vehicular en el estado Táchira. Su compromiso con la atención personalizada y la transparencia la convirtió en la elección preferida de muchos clientes.
                                             <br><br>
-                                        Con el tiempo, se transformó en una sólida institución, comprometida con el bienestar de miles de venezolanos. Hoy en día, la empresa continúa su legado, adaptándose a las nuevas necesidades y desafíos del mundo moderno, siempre con la responsabilidad social y la sostenibilidad como estandartes.
+                                        RECEVECA se mantuvo a la vanguardia, implementando tecnologías y procesos eficientes para agilizar la emisión de pólizas y mejorar la experiencia del cliente. La empresa siempre ha mantenido su esencia familiar, transmitiendo valores como la honestidad, la responsabilidad y el compromiso hacia sus asegurados.
                                             <br><br>
-                                        Su es un ejemplo de cómo una pequeña empresa con visión, compromiso y responsabilidad puede alcanzar grandes cosas.
+                                            Hoy en día, RECEVECA sigue siendo una empresa sólida y confiable, con una amplia cartera de clientes satisfechos y una visión de crecimiento continuo. RECEVECA ofrece una variedad de pólizas de responsabilidad civil vehicular, cubriendo daños a terceros, lesiones personales y daños materiales. Su equipo de profesionales se esfuerza por brindar soluciones adaptadas a las necesidades individuales de cada cliente. 
 
                                     </p>
                                 </div>
