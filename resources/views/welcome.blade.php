cd30ec67ea7b89df5697c42e59675f8a3e7cd103
angelyovanny <angel_yovany@hotmail.com>
2024-06-29 19:46:40 -0400

    modificacion de titulos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
                                         href="{{ route('login') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
-                                        Iniciar Sesión
+                                        INICIAR SECIÓN
                                     </a>
 
                                     @if (Route::has('register'))
@@ -48,7 +48,7 @@
                                             href="{{ route('register') }}"
                                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            Registrarse
+                                            REGISTRARSE
                                         </a>
                                     @endif
                                 @endauth
